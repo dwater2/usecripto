ef93e20043e6b59ec7c60e6c6ecc23452e36a921
Cristian <crisventuri220@gmail.com>
2018-09-02 14:27:20 -0300

    Removido arquivos desnecessários

--- back/src/models/transaction.js
+++ back/src/models/transaction.js
@@ -3,8 +3,8 @@ function Transaction(){
 
 Transaction.prototype.saveTransaction = function(app, de, para, valor, data){
     var userModel = new app.src.models.user();
-    userModel.incrementaSaldo(app, de, valor, data);
-    userModel.decrementaSaldo(app, para, valor, data);
+    userModel.incrementaSaldo(app, para, valor, data);
+    userModel.decrementaSaldo(app, de, valor, data);
 
     var item = {
         "de": de,
